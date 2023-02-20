d269755e1bd82d060a4c991b65346fa82019306e
Roman <romanlabzin03@gmail.com>
2023-02-20 15:55:09 +0500

    1.9 начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    // Указываем путь до входной точки:
+    entry: './src/main.js',
+    // Описываем, куда следует поместить результат работы:
+    output: {
+        // Путь до директории (важно использовать path.resolve):
+        path: path.resolve(__dirname, 'build'),
+        // Имя файла со сборкой:
+        filename: 'bundle.js',
+        clean: true,
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'public' }]
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+}
